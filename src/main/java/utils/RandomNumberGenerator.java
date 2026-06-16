42d2af353af2eacbb65fe66decb4fec41000742a
minhkhoa1010 <163806529+minhkhoa1010@users.noreply.github.com>
2026-06-17 01:22:09 +0700

    refactor RandomNumberGenerator

--- src/main/java/utils/RandomNumberGenerator.java
+++ src/main/java/utils/RandomNumberGenerator.java
@@ -7,23 +7,14 @@ public class RandomNumberGenerator {
     private static final int OTP_LENGTH = 6;
     private static final Random RANDOM = new Random();
 
-    public static String generateRandomNumbersString() {
-        StringBuilder randomNumbersString = new StringBuilder();
 
-        for (int i = 0; i < OTP_LENGTH; i++) {
-            int randomNumber = RANDOM.nextInt(10);
-            randomNumbersString.append(randomNumber);
-        }
-
-        return randomNumbersString.toString();
-    }
 
     public static void main(String[] args) {
         int count = 6;
-        String randomNumbersString = generateRandomNumbersString();
+       // String randomNumbersString = generateRandomNumbersString();
 
         System.out.println("Generated Random Numbers:");
-        System.out.println(randomNumbersString);
+       // System.out.println(randomNumbersString);
     }
 }
 
